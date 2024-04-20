ca9793fc360005a617e4548c8780e6d370496237
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-04-20 19:01:49 +0900

    Update: Changed so that file paths can be obtained from command line arguments

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 // version 1.0
+#include <stdio.h>
+#include <stdlib.h>
 #include "asmx.h"
-int main(void) {
-    const char *file = "sample.yaml";
+int main(const int argc, const char *argv[]) {
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        exit(1);
+    }
+
+    const char *file = argv[1];
+
     ASMX *asmx = newAsmx(file);
     ASMC *asmc = newAsmc(asmx);
 
